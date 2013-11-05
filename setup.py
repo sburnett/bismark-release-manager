17dd1f2242872f3ac9b8ba611c144e5e9af84684
Sam Burnett <srburnet@gmail.com>
2013-11-04 20:28:12 -0500

    Upgrade version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bismark-release-manager',
-    version='1.0.38',
+    version='1.0.39',
     description='Manage releases, packages, experiments and upgrades '
     'for the BISmark deployment.',
     license='MIT License',
