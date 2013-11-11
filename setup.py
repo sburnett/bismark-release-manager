43effd41397e448c5c1f0fa74eae6c1fc9d55f0d
Sam Burnett <srburnet@gmail.com>
2013-11-11 17:38:12 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bismark-release-manager',
-    version='1.0.42',
+    version='1.0.43',
     description='Manage releases, packages, experiments and upgrades '
     'for the BISmark deployment.',
     license='MIT License',
