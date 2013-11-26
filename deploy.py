1dca4b15b19fcff5ace08ab94cc2605852019582
Sam Burnett <srburnet@gmail.com>
2013-11-26 12:33:06 -0500

    Exclude Packages.sig from rsync diff

--- deploy.py
+++ deploy.py
@@ -485,7 +485,7 @@ def _deploy_static(releases_root, deployment_path):
 
 
 def _diff_from_destination(deployment_path, destination):
-    command = 'rsync -n -cvlrz --delete %s/ %s' % (
+    command = 'rsync -n -cvlrz --exclude=Packages.sig --delete %s/ %s' % (
         deployment_path, destination)
     logging.info('Going to run: %s', command)
     return_code = subprocess.call(command, shell=True)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bismark-release-manager',
-    version='1.0.45',
+    version='1.0.46',
     description='Manage releases, packages, experiments and upgrades '
     'for the BISmark deployment.',
     license='MIT License',
